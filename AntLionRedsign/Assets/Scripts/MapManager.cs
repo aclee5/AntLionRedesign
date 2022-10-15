38a2495fded84b22a7ab2a263f0b50de7bfc0be0
errr1n <eteply@telus.net>
2022-10-14 17:50:13 -0700

    tileMap included

---
+++ AntLionRedsign/Assets/Scripts/MapManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap map;
+
+    [SerializeField]
+    private List<TileData> tileDatas;
+
+    private Dictionary<TileBase, TileData> dataFromTiles;
+
+    public void Awake()
+    {
+        Debug.Log("awake");
+        dataFromTiles = new Dictionary<TileBase, TileData>();
+
+        foreach (var tileData in tileDatas)
+        {
+            foreach (var tile in tileData.tiles)
+            {
+                dataFromTiles.Add(tile, tileData);
+            }
+        }
+    }
+
+    void Update()
+    {
+        Debug.Log("update");
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = map.WorldToCell(mousePosition);
+
+            TileBase clickedTile = map.GetTile(gridPosition);
+
+            float timeAdd = dataFromTiles[clickedTile].timeAdd;
+
+
+            print("Timer add on " + clickedTile + " is " + timeAdd);
+
+        }
+    }
+}
