33935f8168240bf4c6dbd6e5bdb65d8a9bfba19b
Alexis Lee <aclee2001@gmail.com>
2022-10-22 22:26:20 -0700

    Implement Slider for Volume Control

---
+++ AntLionRedsign/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    
+    [SerializeField] AudioMixer mixer;
+    
+    public const string SFX_KEY = "sfxVolume"; 
+
+    void Awake(){
+        if (instance == null){
+            instance = this;
+        }
+        else{
+            Destroy(gameObject);
+        }
+        LoadVolume(); 
+    }
+
+    void LoadVolume(){
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_KEY, 1f);
+        mixer.SetFloat(VolumeSettings.MIXER_SFX, Mathf.Log10(sfxVolume)*20);
+
+    }
+}
